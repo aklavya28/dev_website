04f165ec14f4650e53d64aa40c3469f7edcb1e76
aklavya28 <aklavya28@gmail.com>
2024-12-02 12:18:08 +0530

    privacy done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { SchemesComponent } from './pages/schemes/schemes.component';
+import { PrivacyComponent } from './pages/privacy/privacy.component';
 
 export const routes: Routes = [
     {path: "", component: HomeComponent},
     {path: "about", component: AboutComponent},
     {path: "plans", component: SchemesComponent},
     {path: "contact", component: ContactComponent},
+    {path: "privacy", component: PrivacyComponent},
     {
         path: '**',
         redirectTo: '',
---
+++ src/app/pages/privacy/privacy.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderBrandComponent } from '@coreui/angular';
+import { HeaderTitleComponent } from '../../layout/header/header-title/header-title.component';
+
+@Component({
+  selector: 'app-privacy',
+  imports: [HeaderTitleComponent],
+  templateUrl: './privacy.component.html',
+  styleUrl: './privacy.component.scss'
+})
+export class PrivacyComponent {
+
+}
