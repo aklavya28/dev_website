d838d083ba79fe041a2f58c280243be0b5087cf1
aklavya28 <aklavya28@gmail.com>
2024-12-02 14:30:28 +0530

    done website changes

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -29,24 +29,20 @@ export class HomeComponent implements OnInit {
   // /assets/images/banner2.jpg
   slides: any[] = new Array(3).fill({ id: -1, src: '', title: '', subtitle: '' });
   ngOnInit(): void {
-    this.slides[0] = {
-      id: 0,
-      src: '/assets/images/banner1.jpg',
-      title: 'First slide',
-      subtitle: 'Nulla vitae elit libero, a pharetra augue mollis interdum.'
-    };
-    this.slides[1] = {
+    this.slides = [{
       id: 1,
       src: 'assets/images/banner2.jpg',
       title: 'Second slide',
       subtitle: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    };
-    this.slides[2] = {
+    },
+    {
       id: 2,
       src: 'assets/images/banner_1.jpg',
       title: 'Third slide',
       subtitle: 'Praesent commodo cursus magna, vel scelerisque nisl consectetur.'
-    };
+    }
+  ]
+
   }
 
 }
